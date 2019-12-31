5b27f4868ec37f93aa6099a298ff4f4e0c9fac24
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 09:42:16 -0600

    Rewriting as Laminas Project package

--- test/AuraRouter/ConfigProviderTest.php
+++ test/AuraRouter/ConfigProviderTest.php
@@ -1,18 +1,19 @@
 <?php
+
 /**
- * @see       https://github.com/zendframework/zend-expressive-aurarouter for the canonical source repository
- * @copyright Copyright (c) 2018 Zend Technologies USA Inc. (https://www.zend.com)
- * @license   https://github.com/zendframework/zend-expressive-aurarouter/blob/master/LICENSE.md New BSD License
+ * @see       https://github.com/mezzio/mezzio-aurarouter for the canonical source repository
+ * @copyright https://github.com/mezzio/mezzio-aurarouter/blob/master/COPYRIGHT.md
+ * @license   https://github.com/mezzio/mezzio-aurarouter/blob/master/LICENSE.md New BSD License
  */
 
 declare(strict_types=1);
 
-namespace ZendTest\Expressive\Router\AuraRouter;
+namespace MezzioTest\Router\AuraRouter;
 
+use Mezzio\Router\AuraRouter;
+use Mezzio\Router\AuraRouter\ConfigProvider;
+use Mezzio\Router\RouterInterface;
 use PHPUnit\Framework\TestCase;
-use Zend\Expressive\Router\AuraRouter;
-use Zend\Expressive\Router\AuraRouter\ConfigProvider;
-use Zend\Expressive\Router\RouterInterface;
 
 class ConfigProviderTest extends TestCase
 {
