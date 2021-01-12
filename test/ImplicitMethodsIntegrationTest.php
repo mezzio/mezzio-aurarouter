7672a63be19d974ae68b44bd430ffb1f1be15949
Jens de Nies <j.de.nies@protonmail.com>
2021-01-12 23:27:28 +0100

    Used "ImplicitMethodsIntegrationTest" instead of "AbstractImplicitMethodsIntegrationTest" because the former is not available in older releases.
    Signed-off-by: Jens de Nies <j.de.nies@protonmail.com>

--- test/ImplicitMethodsIntegrationTest.php
+++ test/ImplicitMethodsIntegrationTest.php
@@ -13,7 +13,7 @@ namespace MezzioTest\Router;
 use Generator;
 use Mezzio\Router\AuraRouter;
 use Mezzio\Router\RouterInterface;
-use Mezzio\Router\Test\AbstractImplicitMethodsIntegrationTest as RouterIntegrationTest;
+use Mezzio\Router\Test\ImplicitMethodsIntegrationTest as RouterIntegrationTest;
 use Prophecy\PhpUnit\ProphecyTrait;
 
 class ImplicitMethodsIntegrationTest extends RouterIntegrationTest
